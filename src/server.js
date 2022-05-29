b9a0d404b3b66cc30f681bfec3dc56f60dd21e17
Navaneet1999 <93372592+Navaneet1999@users.noreply.github.com>
2022-05-30 02:33:33 +0530

    server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const app = require("./index");
 const server = require("./configs/db");
-
+const port = process.env.PORT || 3000
 // process.env.PORT
-app.listen(process.env.PORT || 3000, async (req, res) => {
+app.listen(port, async (req, res) => {
   try {
     await server();
-    console.log("Default port is active on PORT");
+    console.log("Default port is active on PORT",port);
   } catch (e) {
     console.log("e:", e);
   }
