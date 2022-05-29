6d6606d761e8975c663ea67048fc603bdba2a608
Navaneet1999 <93372592+Navaneet1999@users.noreply.github.com>
2022-05-30 02:03:10 +0530

    port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = require("./index");
 const server = require("./configs/db");
 
 // process.env.PORT
-app.listen(process.env.PORT || 5000, async (req, res) => {
+app.listen(process.env.PORT || 3000, async (req, res) => {
   try {
     await server();
     console.log("Default port is active on PORT");
